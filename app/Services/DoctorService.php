5d33c46e1971f21c8cbaa6d38cbf3cf2fdf3953d
a3mqq2 <aishaaltery03@gmail.com>
2025-05-06 11:55:35 +0200

    updates

--- app/Services/DoctorService.php
+++ app/Services/DoctorService.php
@@ -108,7 +108,7 @@ class DoctorService
          if(request('regestration'))
          {
             $query->where('code', null);
-            $query->where('status', 'init_approve');
+            $query->where('membership_status','!=', 'init_approve');
          }
 
          if(request('init_approve'))
