7bf233be15ba2cb68a5cecdc10fff2736a8e8663
a3mqq2 <aishaaltery03@gmail.com>
2025-05-07 19:40:53 +0200

    updates

--- app/Console/Commands/passport.php
+++ app/Console/Commands/passport.php
@@ -7,7 +7,7 @@ use App\Models\DoctorFile;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 
-class internship_certificate extends Command
+class passport extends Command
 {
     /**
      * The name and signature of the console command.
