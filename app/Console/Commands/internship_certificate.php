0f4201e06b96628aaff8b8892f76df06c69e868e
a3mqq2 <aishaaltery03@gmail.com>
2025-05-07 19:38:14 +0200

    updates

---
+++ app/Console/Commands/internship_certificate.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Doctor;
+use App\Models\DoctorFile;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class internship_certificate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:internship_certificate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $branchId = 1;
+        
+        $doctors = Doctor::where('branch_id', $branchId)
+            ->whereNotNull('doctor_number')
+            ->orderBy('doctor_number')
+            ->get();
+
+        $this->info("Found {$doctors->count()} doctors.");
+
+        foreach ($doctors as $doctor) {
+            $fileName = "internship_certificate.jpg";
+            $filePath = "documents/{$doctor->doctor_number}/{$fileName}";
+
+            if (Storage::disk('public')->exists($filePath)) {
+
+                DoctorFile::create([
+                    'doctor_id'    => $doctor->id,
+                    'file_name'    => $fileName,
+                    'file_path'    => $filePath,
+                    'file_type_id' => 28,
+                ]);
+
+                $this->info("Image imported for Doctor ID: {$doctor->id} - {$fileName}");
+            } else {
+                $this->warn("Image not found for Doctor ID: {$doctor->id} - Expected: {$fileName}");
+            }
+        }
+
+        $this->info('All doctor images processed successfully.');
+    }
+}
